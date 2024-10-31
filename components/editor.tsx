2d631cdd0d613db5c0f6f0e1175234e416f8b246
SyedAbdulRehman1 <fast33210@gmail.com>
2024-11-01 01:28:36 +0500

    chapter-description issue fixes

--- components/editor.tsx
+++ components/editor.tsx
@@ -3,7 +3,8 @@
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
-import "react-quill/dist/quill.snow.css";
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -14,7 +15,11 @@ export const Editor = ({
   onChange,
   value,
 }: EditorProps) => {
-  const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
+  // // const ReactQuill = useMemo(() => dynamic(() => import("react-quill-new"), { ssr: false }), []);
+  // const ReactQuill = dynamic(() => import("react-quill-new"), {
+  //   ssr: false, // Disable SSR for this component
+  // });
+  const ReactQuill = useMemo(() => dynamic(() => import("react-quill-new"), { ssr: false }), []);
 
   return (
     <div className="bg-white">
