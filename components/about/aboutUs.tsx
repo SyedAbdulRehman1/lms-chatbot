733a19c964af9c66d44700abd7c231745e2899d2
Huzaifa Hanif <huzaifahanif184@gmail.com>
2024-11-09 16:37:00 +0500

    about page added

--- components/about/aboutUs.tsx
+++ components/about/aboutUs.tsx
@@ -1,23 +1,24 @@
 // import "../img/carousel-1.jpg"
+import Link from "next/link";
 import Image from "next/image";
 export default function AboutTopSection() {
   return (
     <>
-      <div class="container-fluid bg-primary py-5 mb-5 page-header">
-      <div class="container py-5">
-        <div class="row justify-content-center">
-          <div class="col-lg-10 text-center">
-            <h1 class="display-3 text-white animated slideInDown">About Us</h1>
+      <div className="container-fluid bg-primary py-5 mb-5 page-header">
+      <div className="container py-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-10 text-center">
+            <h1 className="display-3 text-white animated slideInDown">About Us</h1>
             <nav aria-label="breadcrumb">
-              <ol class="breadcrumb justify-content-center">
-                <li class="breadcrumb-item">
-                  <a class="text-white" href="#">Home</a>
+              <ol className="breadcrumb justify-content-center">
+                <li className="breadcrumb-item">
+                  < Link className="text-white" href="#">Home</Link>
                 </li>
-                <li class="breadcrumb-item">
-                  <a class="text-white" href="#">Pages</a>
+                <li className="breadcrumb-item">
+                  <Link className="text-white" href="#">Pages</Link>
                 </li>
                 <li
-                  class="breadcrumb-item text-white active"
+                  className="breadcrumb-item text-white active"
                   aria-current="page"
                 >
                   About
---
+++ components/about/service.tsx
@@ -0,0 +1,63 @@
+// import "../img/carousel-1.jpg"
+import Link from "next/link";
+import Image from "next/image";
+export default function Service() {
+  return (
+    <>
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-4">
+            <div
+              className="col-lg-3 col-sm-6 wow fadeInUp"
+              data-wow-delay="0.1s"
+            >
+              <div className="service-item text-center pt-3">
+                <div className="p-4">
+                  <i className="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
+                  <h5 className="mb-3">Skilled Instructors</h5>
+                  <p>From all over he world</p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-lg-3 col-sm-6 wow fadeInUp"
+              data-wow-delay="0.3s"
+            >
+              <div className="service-item text-center pt-3">
+                <div className="p-4">
+                  <i className="fa fa-3x fa-globe text-primary mb-4"></i>
+                  <h5 className="mb-3">Online classNamees</h5>
+                  <p>Access the Course Content</p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-lg-3 col-sm-6 wow fadeInUp"
+              data-wow-delay="0.5s"
+            >
+              <div className="service-item text-center pt-3">
+                <div className="p-4">
+                  <i className="fa fa-3x fa-home text-primary mb-4"></i>
+                  <h5 className="mb-3">Assignments</h5>
+                  <p>Assignments and quizes will be conducted accordingly</p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-lg-3 col-sm-6 wow fadeInUp"
+              data-wow-delay="0.7s"
+            >
+              <div className="service-item text-center pt-3">
+                <div className="p-4">
+                  <i className="fa fa-3x fa-book-open text-primary mb-4"></i>
+                  <h5 className="mb-3">Content Library</h5>
+                  <p>Access Books, Notes , Video Lectures and Results</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
