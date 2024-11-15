708893ae55ce3d0e016cafe80953123ce214ed06
syedabdulrehman1 <fast33210@gmail.com>
2024-11-15 10:35:20 +0500

    fixes

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,10 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <Link href={"/dashboard"}>
-      <Image height={130} width={130} alt="logo" src="/logo.svg" />
+    // <Link href={"/dashboard"}>
+    //   <Image height={130} width={130} alt="logo" src="/logo.svg" />
+    // </Link>
+    <Link
+      href="/"
+      className="navbar-brand d-flex align-items-center px-4 px-lg-5"
+    >
+      <h2 className="m-0 text-primary">
+        <FontAwesomeIcon icon={faBook} className="me-3" />
+        LMS
+      </h2>
     </Link>
   );
 };
